d32c345c5083d434c70d16d177a534578004bc7f
RICARDO FREITAS <rmotafreitas@gmail.com>
2024-04-06 22:54:49 +0100

    Update trade form and trade log

--- client/src/components/create-trade.tsx
+++ client/src/components/create-trade.tsx
@@ -15,6 +15,7 @@ import { Input } from "./ui/input";
 import { Slider } from "@/components/ui/slider";
 import { User } from "@/lib/hooks/useAuth";
 import { useEffect, useState } from "react";
+import api from "@/lib/api/api";
 
 interface CreateTradeProps {
   isOpen: boolean;
@@ -43,6 +44,26 @@ export function CreateTrade({
   const [selectedStopLoss, setSelectedStopLoss] = useState<number>(2);
   const [selectedTime, setSelectedTime] = useState<"15m" | "4h" | "1d">("15m");
 
+  const handleSubmit = async () => {
+    const body = {
+      asset: selectedCrypto,
+      spread:
+        selectedCrypto === "BTC"
+          ? BTC.spread_percentage
+          : ETH.spread_percentage,
+      risk: selectedRisk,
+      assetprice: +(selectedCrypto === "BTC" ? BTC.bid : ETH.bid),
+      initialInvestment: selectedActualAmount,
+      takeProfit: selectedTakeProfit,
+      stopLoss: selectedStopLoss,
+      window_money: selectedTime,
+    };
+    console.log(body);
+    const res = await api.post("/trade/open", body);
+    console.log(res);
+    onClose();
+  };
+
   useEffect(() => {
     const value = parseFloat(selectedAmount.toString());
     const spread =
@@ -60,153 +81,160 @@ export function CreateTrade({
   ]);
 
   return (
-    <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="max-w-3xl min-w-max">
-        <DialogHeader>
-          <DialogTitle>Create a trade</DialogTitle>
-          <DialogDescription>
-            This form needs to be filled out to create a trade.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-row gap-4">
-            <div
-              className={`rounded-md overflow-hidden flex-1 flex ${
-                selectedCrypto === "BTC" ? "border-2 border-primary" : ""
-              }`}
-              onClick={() => setSelectedCrypto("BTC")}
-            >
-              <AssetComponent
-                crypto={BTC}
-                selectedCurrency={selectedCurrency}
-                cryptoName="Bitcoin"
-                possibleCurrencies={possibleCurrencies}
-              />
+    <>
+      <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent className="max-w-3xl min-w-max">
+          <DialogHeader>
+            <DialogTitle>Create a trade</DialogTitle>
+            <DialogDescription>
+              This form needs to be filled out to create a trade.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-row gap-4">
+              <div
+                className={`rounded-md overflow-hidden flex-1 flex ${
+                  selectedCrypto === "BTC" ? "border-2 border-primary" : ""
+                }`}
+                onClick={() => setSelectedCrypto("BTC")}
+              >
+                <AssetComponent
+                  crypto={BTC}
+                  selectedCurrency={selectedCurrency}
+                  cryptoName="Bitcoin"
+                  possibleCurrencies={possibleCurrencies}
+                />
+              </div>
+              <div
+                className={`rounded-md overflow-hidden flex-1 flex ${
+                  selectedCrypto === "ETH" ? "border-2 border-primary" : ""
+                }`}
+                onClick={() => setSelectedCrypto("ETH")}
+              >
+                <AssetComponent
+                  crypto={ETH}
+                  selectedCurrency={selectedCurrency}
+                  cryptoName="Etherium"
+                  possibleCurrencies={possibleCurrencies}
+                />
+              </div>
             </div>
-            <div
-              className={`rounded-md overflow-hidden flex-1 flex ${
-                selectedCrypto === "ETH" ? "border-2 border-primary" : ""
-              }`}
-              onClick={() => setSelectedCrypto("ETH")}
-            >
-              <AssetComponent
-                crypto={ETH}
-                selectedCurrency={selectedCurrency}
-                cryptoName="Etherium"
-                possibleCurrencies={possibleCurrencies}
+            <div className="flex flex-row gap-4 justify-start items-center">
+              <Label className="font-semibold text-base">Amount:</Label>
+              <Input
+                className="max-w-xs"
+                type="number"
+                value={selectedAmount}
+                onChange={(e) => {
+                  const value = parseFloat(e.target.value);
+                  const spread =
+                    (selectedCrypto === "BTC"
+                      ? BTC.spread_percentage
+                      : ETH.spread_percentage) / 100;
+                  const aux = value * spread;
+                  const total = value - aux;
+                  setSelectedAmount(value);
+                  setSelectedActualAmount(+Number(+total).toFixed(2));
+                }}
+                placeholder={`Amount in ${selectedCurrency} - ${
+                  possibleCurrencies.find(
+                    (currency) => currency.name === selectedCurrency
+                  )?.token
+                }`}
               />
+              <Label className="ml-auto text-base gap-2 w-fit font-normal">
+                After taxes:{" "}
+                {isNaN(selectedActualAmount) ? 0 : selectedActualAmount}{" "}
+                {
+                  possibleCurrencies.find(
+                    (currency) => currency.name === selectedCurrency
+                  )?.token
+                }
+              </Label>
+            </div>
+            <div className="flex flex-row gap-4 justify-center items-center">
+              <Label className="font-semibold text-base">Risk:</Label>
+              <Slider
+                defaultValue={[selectedRisk]}
+                max={80}
+                step={1}
+                min={5}
+                onValueChange={(value) => {
+                  setSelectedRisk(value[0]);
+                }}
+              />
+              <Label className="font-semibold text-base">{selectedRisk}%</Label>
+            </div>
+            <div className="flex flex-row gap-4 justify-start items-center">
+              <Label className="font-semibold text-base">Take profit:</Label>
+              <Input
+                className="max-w-xs"
+                type="number"
+                min={2}
+                value={selectedTakeProfit}
+                onChange={(e) => {
+                  setSelectedTakeProfit(parseFloat(e.target.value));
+                }}
+                placeholder="%"
+              />
+              <Label className="text-base gap-2 w-fit font-normal">%</Label>
+            </div>
+            <div className="flex flex-row gap-4 justify-start items-center">
+              <Label className="font-semibold text-base">Stop loss:</Label>
+              <Input
+                className="max-w-xs"
+                type="number"
+                min={2}
+                value={selectedStopLoss}
+                onChange={(e) => {
+                  setSelectedStopLoss(parseFloat(e.target.value));
+                }}
+                placeholder="%"
+              />
+              <Label className="text-base gap-2 w-fit font-normal">%</Label>
+            </div>
+            <div className="flex flex-col gap-4 justify-center items-start">
+              <Label className="font-semibold text-base">Time:</Label>
+              <div className="flex flex-row justify-center items-center w-4/5 self-center">
+                <TimeBtn
+                  time="15m"
+                  selectedTime={selectedTime}
+                  setSelectedTime={() => setSelectedTime("15m")}
+                  inOnTheLeft
+                  text="Fast trade"
+                />
+                <TimeBtn
+                  time="4h"
+                  selectedTime={selectedTime}
+                  setSelectedTime={() => setSelectedTime("4h")}
+                  isOnTheMiddle
+                  text="Medium trade"
+                />
+                <TimeBtn
+                  time="1d"
+                  selectedTime={selectedTime}
+                  setSelectedTime={() => setSelectedTime("1d")}
+                  isOnTheRight
+                  text="Slow trade"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex flex-row gap-4 justify-start items-center">
-            <Label className="font-semibold text-base">Amount:</Label>
-            <Input
-              className="max-w-xs"
-              type="number"
-              value={selectedAmount}
-              onChange={(e) => {
-                const value = parseFloat(e.target.value);
-                const spread =
-                  (selectedCrypto === "BTC"
-                    ? BTC.spread_percentage
-                    : ETH.spread_percentage) / 100;
-                const aux = value * spread;
-                const total = value - aux;
-                setSelectedAmount(value);
-                setSelectedActualAmount(+Number(+total).toFixed(2));
-              }}
-              placeholder={`Amount in ${selectedCurrency} - ${
-                possibleCurrencies.find(
-                  (currency) => currency.name === selectedCurrency
-                )?.token
-              }`}
-            />
-            <Label className="ml-auto text-base gap-2 w-fit font-normal">
-              After taxes:{" "}
-              {isNaN(selectedActualAmount) ? 0 : selectedActualAmount}{" "}
-              {
-                possibleCurrencies.find(
-                  (currency) => currency.name === selectedCurrency
-                )?.token
-              }
-            </Label>
-          </div>
-          <div className="flex flex-row gap-4 justify-center items-center">
-            <Label className="font-semibold text-base">Risk:</Label>
-            <Slider
-              defaultValue={[selectedRisk]}
-              max={80}
-              step={1}
-              min={5}
-              onValueChange={(value) => {
-                setSelectedRisk(value[0]);
-              }}
-            />
-            <Label className="font-semibold text-base">{selectedRisk}%</Label>
-          </div>
-          <div className="flex flex-row gap-4 justify-start items-center">
-            <Label className="font-semibold text-base">Take profit:</Label>
-            <Input
-              className="max-w-xs"
-              type="number"
-              min={2}
-              value={selectedTakeProfit}
-              onChange={(e) => {
-                setSelectedTakeProfit(parseFloat(e.target.value));
-              }}
-              placeholder="%"
-            />
-            <Label className="text-base gap-2 w-fit font-normal">%</Label>
-          </div>
-          <div className="flex flex-row gap-4 justify-start items-center">
-            <Label className="font-semibold text-base">Stop loss:</Label>
-            <Input
-              className="max-w-xs"
-              type="number"
-              min={2}
-              value={selectedStopLoss}
-              onChange={(e) => {
-                setSelectedStopLoss(parseFloat(e.target.value));
-              }}
-              placeholder="%"
-            />
-            <Label className="text-base gap-2 w-fit font-normal">%</Label>
-          </div>
-          <div className="flex flex-col gap-4 justify-center items-start">
-            <Label className="font-semibold text-base">Time:</Label>
-            <div className="flex flex-row justify-center items-center w-4/5 self-center">
-              <TimeBtn
-                time="15m"
-                selectedTime={selectedTime}
-                setSelectedTime={() => setSelectedTime("15m")}
-                inOnTheLeft
-                text="Fast trade"
-              />
-              <TimeBtn
-                time="4h"
-                selectedTime={selectedTime}
-                setSelectedTime={() => setSelectedTime("4h")}
-                isOnTheMiddle
-                text="Medium trade"
-              />
-              <TimeBtn
-                time="1d"
-                selectedTime={selectedTime}
-                setSelectedTime={() => setSelectedTime("1d")}
-                isOnTheRight
-                text="Slow trade"
-              />
-            </div>
-          </div>
-        </div>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button onClick={onClose} type="submit">
-              Create Trade
-            </Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button
+                onClick={() => {
+                  handleSubmit();
+                }}
+                type="submit"
+              >
+                Create Trade
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
 
