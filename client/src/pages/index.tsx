b8c6eeaa002772d594fb18db8e6a5fc3c8b6c06c
RICARDO FREITAS <rmotafreitas@gmail.com>
2024-04-07 10:27:19 +0100

    Update homepage content and add new feature to display millionaire status

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,7 +7,7 @@ export function HomePage() {
       <div className="w-full bg-inherit bg-no-repeat bg-cover justify-center items-center flex flex-row max-sm:flex-col gap-10 flex-wrap">
         <div className="max-w-3xl flex flex-col justify-center items-start gap-3 p-10">
           <h1 className="text-4xl font-bold">
-            Win money by doing absolutely nothing!
+            Win money by doing absolutely nothing!{" "}
             <span className="text-primary">Be millionaire in no time!</span>
           </h1>
           <h2 className="text-2xl text-foreground opacity-70">
